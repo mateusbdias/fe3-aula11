b27eac488cdc6308e8cc1a227ddd0ec6bfff2a43
Mateus Benites Dias <imattdias@gmail.com>
2022-03-22 15:27:24 -0300

    CTD - Front End III - Aula 11 - Exercício Sync

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import { useState } from "react";
 
+import { Formik } from "formik";
+
 function App() {
-
-  const [nome, setNome] = useState("");
-  const [curso, setCurso] = useState("back-end");
-
-  function submitForm(event) {
-    event.preventDefault();
-    console.log(nome + " - " + curso);
-  }
-
   return (
-    <form onSubmit={submitForm}>
-      <h1>{nome} - {curso}</h1>
-      <input 
-        placeholder="Nome" 
-        onChange={(event) => setNome(event.target.value)} 
-      />
-      <select 
-        onChange={(event) => setCurso(event.target.value)}
-        defaultValue={curso}
-      >
-        <option></option>
-        <option value="back-end">Back End</option>
-        <option value="devops">DevOps</option>
-        <option value="front-end">Front End</option>
-      </select>
-      <input type="submit" />
-    </form>
+    <Formik
+      // Valores iniciais do form 
+      initialValues={{ email: "" }}
+      // Função de submissão do form
+      onSubmit={(values) => alert(values.email)}
+      // Validação de campos
+      validate={(values) => {
+        const errors = {};
+
+        if(!values.email){
+          errors.email = "Campo obrigatório";
+        } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
+          errors.email = "Email inválido";
+        }
+
+        return errors;
+      }}
+    >
+      {({ handleSubmit, handleChange, errors }) => (
+        <form onSubmit={handleSubmit}>
+          <input 
+            name="email" 
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          {errors.email && <span>{errors.email}</span>}
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </Formik>
   );
 }
 
